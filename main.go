bf9a0569cf24737f9af56e6bf13ed59aefa1c8dc
Pedro Costa <dev@iampfac.com>
2020-04-09 18:57:47 +0100

    Read lines from standard input until end-of-file

--- main.go
+++ main.go
@@ -3,20 +3,43 @@ package main
 import (
   "bufio"
   "fmt"
+  "io"
   "log"
   "os"
 )
 
+func ReadLine(reader *bufio.Reader) (string, error) {
+  var line, part []byte
+  var isPrefix bool = true
+  var err error = nil
+
+  for isPrefix && err == nil {
+    part, isPrefix, err = reader.ReadLine()
+    line = append(line, part...)
+  }
+
+  return string(line), err
+}
+
+func ConvertCommitmentPieceToCID(commp string) (string) {
+  return commp
+}
+
 func main() {
-  reader := bufio.NewReader(os.Stdin)
+  var reader = bufio.NewReader(os.Stdin)
+  var commp string
+  var err error = nil
 
-  fmt.Println("Who are you?")
+  for err == nil {
+    commp, err = ReadLine(reader)
+    if err == nil {
+      cid := ConvertCommitmentPieceToCID(commp)
+      fmt.Println(cid)
+    }
+  }
 
-  name, _, err := reader.ReadLine()
-  if err != nil {
+  if err != io.EOF {
     log.Fatal("Failed to read line from standard input: %w\n", err)
     os.Exit(1)
   }
-
-  fmt.Printf("Hello %s\n", name)
 }
