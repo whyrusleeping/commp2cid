2d948cfde5f09f08ccd824af2182b56f6274dedc
Jeromy <jeromyj@gmail.com>
2020-04-22 09:00:04 -0700

    hex decode input strings

--- main.go
+++ main.go
@@ -1,42 +1,30 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "io"
-  "log"
-  "os"
-  commcid "github.com/filecoin-project/go-fil-commcid"
+	"bufio"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"os"
+
+	commcid "github.com/filecoin-project/go-fil-commcid"
 )
 
-func ReadLine(reader *bufio.Reader) ([]byte, error) {
-  var line, part []byte
-  var isPrefix bool = true
-  var err error = nil
-
-  for isPrefix && err == nil {
-    part, isPrefix, err = reader.ReadLine()
-    line = append(line, part...)
-  }
-
-  return line, err
-}
-
 func main() {
-  var reader = bufio.NewReader(os.Stdin)
-  var commp []byte
-  var err error = nil
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		commp, err := hex.DecodeString(scanner.Text())
+		if err != nil {
+			log.Fatalf("Failed to hex decode line (%s): %s", scanner.Text(), err)
+			os.Exit(1)
+		}
 
-  for err == nil {
-    commp, err = ReadLine(reader)
-    if err == nil {
-      cid := commcid.PieceCommitmentV1ToCID(commp)
-      fmt.Println(cid)
-    }
-  }
+		cid := commcid.PieceCommitmentV1ToCID(commp)
+		fmt.Println(cid)
+	}
 
-  if err != io.EOF {
-    log.Fatal("Failed to read line from standard input: %w\n", err)
-    os.Exit(1)
-  }
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Failed to read line from standard input: %s", err)
+		os.Exit(1)
+	}
 }
